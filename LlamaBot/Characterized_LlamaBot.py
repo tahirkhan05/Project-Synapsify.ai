89ff3c96ba297df0c5abab77268d876591280cff
Khan <158294781+uniquehorn01@users.noreply.github.com>
2024-05-14 12:05:52 +0530

    Add files via upload

---
+++ LlamaBot/Characterized_LlamaBot.py
@@ -0,0 +1,43 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('LLAMA_API_KEY')
+
+client = Groq(
+  api_key = API_KEY
+)
+
+history = []
+system_msg = {
+  "role": "system",
+  "content": "respond as if you're explaining things to a professional coder."
+}
+history.append(system_msg)
+
+def ask(question):
+  question_msg = {
+    "role": "user",
+    "content": question
+  }
+  history.append(question_msg)
+  response = client.chat.completions.create(
+    messages=history,
+    model="llama3-8b-8192"
+  )
+  answer_msg = response.choices[0].message
+  history.append(answer_msg)
+  return answer_msg.content
+
+while(True):
+  question = input("You: ")
+
+  if(question.strip() == ''):
+    break
+
+  answer = ask(question)
+  print('\n')
+  print(f"Llama3: {answer}")
+  print('\n')
