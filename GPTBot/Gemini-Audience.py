2fc0e7d4ba71f7df9d583017c408ec1ba66d9294
Khan <158294781+uniquehorn01@users.noreply.github.com>
2024-05-18 17:04:51 +0530

    Add files via upload

---
+++ GPTBot/Gemini-Audience.py
@@ -0,0 +1,27 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('GEMINI_API_KEY')
+
+genai.configure(
+    api_key = API_KEY
+)
+
+model = genai.GenerativeModel('gemini-1.5-pro-latest')
+chat = model.start_chat(history=[])
+audience = input("audience: ")
+instruction = f"In this chat, respond as if you're explaining things to a {audience}. "
+
+while(True):
+  question = input("You:  ")
+
+  if(question.strip() == ''):
+    break
+
+  response = chat.send_message(question)
+  print('\n')
+  print(f"Gemini: {response.text}")
+  print('\n')
