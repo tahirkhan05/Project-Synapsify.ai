1edcff08e61ecf22aa18e139b5564f2d17995f98
Khan <158294781+uniquehorn01@users.noreply.github.com>
2024-05-18 17:05:35 +0530

    Add files via upload

---
+++ GPTBot/GPT-Audience.py
@@ -0,0 +1,44 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('OPENAI_API_KEY')
+
+client = OpenAI(
+  api_key=API_KEY
+)
+
+history = []
+audience = input("audience: ")
+system_msg = {
+  "role": "system",
+  "content": f"respond as if you're explaining things to a {audience}."
+}
+history.append(system_msg)
+
+def ask(question):
+  question_msg = {
+    "role": "user",
+    "content": question
+  }
+  history.append(question_msg)
+  response = client.chat.completions.create(
+    messages=history,
+    model="gpt-3.5-turbo"
+  )
+  answer_msg = response.choices[0].message
+  history.append(answer_msg)
+  return answer_msg.content
+
+while(True):
+  question = input("You: ")
+
+  if(question.strip() == ''):
+    break
+
+  answer = ask(question)
+  print('\n')
+  print(f"GPT: {answer}")
+  print('\n')
