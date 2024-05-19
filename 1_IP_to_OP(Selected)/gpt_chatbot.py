c5f7506dc677d2e2233e5bf3ca0bc774adf17b81
Khan <158294781+uniquehorn01@users.noreply.github.com>
2024-05-19 18:36:22 +0530

    gpt_chatbot.py
    now multi-turn

--- 1_IP_to_OP(Selected)/gpt_chatbot.py
+++ 1_IP_to_OP(Selected)/gpt_chatbot.py
@@ -1,15 +1,17 @@
 from openai import OpenAI
 
-def get_gpt_response(audience, question, api_key):
+def get_gpt_response(audience, question, api_key, history):
     client = OpenAI(api_key=api_key)
     instruction = f"In this chat, respond as if you're explaining things to a {audience}. "
     question_msg = {
         "role": "user",
         "content": instruction + question
     }
+    history.append(question_msg)
     response = client.chat.completions.create(
-        messages=[question_msg],
+        messages=history,
         model="gpt-3.5-turbo"
     )
     answer_msg = response.choices[0].message
-    return answer_msg.content
+    history.append(answer_msg)
+    return answer_msg.content, history
