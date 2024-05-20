2e69ece3491ef1786664b2dfd4e3ccf2dd244f90
Khan <158294781+uniquehorn01@users.noreply.github.com>
2024-05-20 17:36:32 +0530

    Delete Demo2 directory

--- Demo2/chatbot_project/chatbot/urls.py
+++
@@ -1,8 +0,0 @@
-# chatbot/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Example URL pattern
-    path('get_response/', views.get_response, name='get_response'),
-]
